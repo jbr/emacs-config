25238ec9e4adbbead70cb5bfb8af0edb4e5583a9
Jacob Rothstein <github@jacobrothstein.com>
2011-02-08 19:28:10 -0800

    [ customize linum face ]

--- custom.el
+++ custom.el
@@ -6,10 +6,10 @@
  '(fringe-mode (quote (0 . 4)) nil (fringe))
  '(global-linum-mode t)
  '(global-undo-tree-mode t)
- '(linum-format "%d  "))
+ '(linum-format "%4d "))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- )
+ '(linum ((t (:inherit (shadow default) :background "gray95" :foreground "gray60")))))
