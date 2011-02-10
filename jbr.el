8c660f7cfb4763ab8f2161ef681a1d19b26dab52
Jacob Rothstein <github@jacobrothstein.com>
2011-02-10 13:12:55 -0800

    [ default size, font, truncate lines ]

--- jbr.el
+++ jbr.el
@@ -1,3 +1,16 @@
+(when window-system
+  (add-to-list 'default-frame-alist '(width . 203))
+  (add-to-list 'default-frame-alist '(height . 62))
+  (set-default-font
+   (concat "-apple-Anonymous_Pro-"
+	   "medium-normal-normal-"
+	   "*-12-*-*-*-m-0-iso10646-1")))
+
+(setq-default truncate-lines t)
+(setq make-backup-files nil
+      auto-save-default nil)
+
+
 (global-set-key (kbd "<s-return>") 'ns-toggle-fullscreen)
 (global-set-key (kbd "M-SPC") 'hippie-expand)
 (setq default-directory "~/code")
@@ -11,3 +24,4 @@
 (require 'peepopen)
 (setq ns-pop-up-frames nil)
 (peepopen-bind-ns-keys)
+
